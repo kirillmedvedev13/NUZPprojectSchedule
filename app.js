2ab6a6719ab6c8b86116c4abd0b4a2d103e4c686
Vlad Primakov <primakov2002@gmail.com>
2021-12-26 13:19:29 +0200

    create app

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("OK")
+const express = require("express")
+const config = require("config")
+const mysql = require("mysql")
+
+const PORT = config.get('port') || 5000
+
+const app = express()
+
+const connection = mysql.createConnection({
+    host     : 'nuzp-shedule.mysql.database.azure.com',
+    user     : 'nuzp_admin',
+    password : 'Morality351973',
+    database : 'shedule',
+    port: 3306
+  })
+   
+connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting to mysql: ' + err.stack);
+      process.exit(1);
+    }
+})
+   
+
+app.listen(PORT, () => console.log(`App has been started on PORT ${PORT}`))
