94b696184c0ac4e095dbcc6f5b1fb9a971778e5d
Vlad Primakov <primakov2002@gmail.com>
2022-01-11 17:00:31 +0200

    12

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import { Fragment } from "react";
 import NaviBar from "./Components/Navbar/Navibar";
+import React from "react";
 
-const App = ({children}) =>  (
-  <Fragment>
-    <NaviBar />
-    {children}
-  </Fragment>
-)
+
+
+class App extends React.Component{
+  state = {
+    openLogin: false,
+  }
+  render(){
+    console.log(this)
+      return(
+        <Fragment>
+          <NaviBar />
+          {this.props.children}
+      </Fragment>
+      )
+  }
+}
+
 
 export default App;
