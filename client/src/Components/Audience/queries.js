7fc4ed7bdc6e8bee8363074a6da5c2bdef2b06ea
Vlad Primakov <primakov2002@gmail.com>
2022-01-17 19:03:20 +0200

    tesd

---
+++ client/src/Components/Audience/queries.js
@@ -0,0 +1,21 @@
+import { gql } from "@apollo/client";
+export const GET_ALL_GROUPS = gql`
+    query ($name: String, $id_cathedra: Int) {
+    GetAllAudiences(name: $name, id_cathedra: $id_cathedra) {
+      id
+      name
+      capacity
+      type_class{
+        id
+        name
+      }
+      assigned_audiences {
+        id
+        cathedra{
+          id
+          name
+        }
+      }
+    }
+  }
+`;
--- server/Schema/Queries/Audience.js
+++ server/Schema/Queries/Audience.js
@@ -1,22 +1,51 @@
-import { GraphQLList } from "graphql";
+import { GraphQLInt, GraphQLList, GraphQLString } from "graphql";
 import db from "../../database.js";
 import AudienceType from "../TypeDefs/AudienceType.js";
+import sequelize, { Op } from "sequelize"
 
 export const GET_ALL_AUDIENCES = {
   type: new GraphQLList(AudienceType),
-  async resolve() {
-    let res =  await db.audience.findAll({
-      include: [{
-        model: db.type_class,
-      },
-      {
-        model: db.assigned_audience,
-        include: {
-          model: db.cathedra,
+  args: {
+    name: { type: GraphQLString },
+    id_cathedra: { type: GraphQLInt },
+  },
+  async resolve(parent, { name, id_cathedra }) {
+    let FilterName = ""
+    let FilterCathedra = ""
+    if (name) {
+      const arr = name.split(" ");
+      let str = ""
+      arr.map((word, index) => {
+        if (index != arr.length - 1) {
+          str += `${word}|`
         }
-      }
-    ]
-    });
-    return res;
+        else {
+          str += word
+        }
+      });
+      FilterName = `Where audiences.name REGEXP '${str}'`
+    }
+    if (id_cathedra) {
+      FilterCathedra = `    
+      join assigned_audiences au2 on au2.id_audience = audiences.id
+      join cathedras c2 on au2.id_cathedra = c2.id and c2.id = ${id_cathedra} 
+      `
+    }
+    
+    db.Connection.query(`
+    select audiences.id, audiences.name, audiences.capacity, audiences.id_type_class, type_classes.name as name_type_class, 
+    group_concat(c1.name separator '||') as "listCathedras", 
+    group_concat(au1.id separator '||') as "listIdsAU" 
+    from audiences
+    join assigned_audiences au1 on au1.id_audience = audiences.id
+    join cathedras c1 on au1.id_cathedra = c1.id
+    join type_classes on audiences.id_type_class = type_classes.id
+    ${FilterCathedra}
+    ${FilterName}
+    group by audiences.id
+    `).then(([res, meta]) => {
+      console.log(res)
+      return res;
+    })
   },
 };
