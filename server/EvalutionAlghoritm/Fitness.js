098c30b04384bd4ab9eca8c62401d6ed3322a57c
Kirill13 <kirillmedvedev279@gmail.com>
2022-04-27 19:44:23 +0300

    Start Fitness

---
+++ server/EvalutionAlghoritm/Fitness.js
@@ -0,0 +1,78 @@
+export default function fitnessByGroups(
+  individ,
+  groups,
+  classes,
+  max_day,
+  max_pair
+) {
+  groups.forEach((group) => {
+    let fitnessValue = 0;
+    let detectedAG = [];
+    classes.map((clas) => {
+      clas.assigned_groups.map((ag) => {
+        if (ag.id_group == group.id) detectedAG.push(ag.id);
+      });
+    });
+    let detectedSchedules = individ.filter((schedule) => {
+      if (detectedAG.indexOf(schedule.id_assigned_group) != -1) {
+        return schedule;
+      }
+    });
+    detectedSchedules.sort(function (schedule1, schedule2) {
+      if (schedule1.id_day_week > schedule2.id_day_week) return 1;
+      else if (schedule1.id_day_week == schedule2.id_day_week) {
+        if (schedule1.number_pair > schedule2.number_pair) return 1;
+        else if (schedule1.number_pair == schedule2.number_pair) {
+          if (schedule1.id_pair_type > schedule2.id_pair_type) return 1;
+          else if (schedule1.id_pair_type == schedule2.id_pair_type) {
+          } else return -1;
+        } else return -1;
+      } else return -1;
+    });
+    let index = 0;
+    for (let i = 0; i < max_day; i++) {
+      for (let j = 0; j < max_pair; j++) {
+        if (
+          detectedSchedules[index].id_day_week == i &&
+          detectedSchedules[index].number_pair == j
+        ) {
+          if (index != 0) {
+            if (
+              detectedSchedules[index - 1].id_day_week ==
+                detectedSchedules[index].id_day_week &&
+              detectedSchedules[index - 1].number_pair !=
+                detectedSchedules[index].number_pair
+            ) {
+              if (
+                detectedSchedules[index - 1].pair_type ==
+                  detectedSchedules[index].pair_type ||
+                detectedSchedules[index].pair_type == 3
+              ) {
+                if (index > 1) {
+                  if (
+                    detectedSchedules[index].pair_type == 3 &&
+                    detectedSchedules[index - 2].number_pair ==
+                      detectedSchedules[index - 1].number_pair &&
+                    detectedSchedules[index - 2].id_day_week ==
+                      detectedSchedules[index].id_day_week
+                  ) {
+                    fitnessValue +=
+                      detectedSchedules[index].number_pair -
+                      detectedSchedules[index - 2].number_pair -
+                      1;
+                  }
+                }
+                fitnessValue +=
+                  detectedSchedules[index].number_pair -
+                  detectedSchedules[index - 1].number_pair -
+                  1;
+              }
+            }
+          }
+          index++;
+        }
+      }
+    }
+    console.log(fitnessValue);
+  });
+}
--- server/EvalutionAlghoritm/Main.js
+++ server/EvalutionAlghoritm/Main.js
@@ -1,45 +1,58 @@
 import db from "../database.js";
 import MessageType from "../Schema/TypeDefs/MessageType.js";
+import fitnessByGroups from "./Fitness.js";
 import Init from "./Init.js";
 
 export const RUN_EA = {
   type: MessageType,
   async resolve(parent, { data, id_cathedra }) {
-    const info = await db.info.findAll(); 
+    const info = await db.info.findAll();
     const max_day = info[0].dataValues.max_day;
     const max_pair = info[0].dataValues.max_pair;
     const population_size = 500;
     const p_crossover = 0.9;
     const p_mutation = 0.1;
     const classes = await db.class.findAll({
-        include: [{
-            model: db.assigned_group,
-            required: true,
+      include: [
+        {
+          model: db.assigned_group,
+          required: true,
         },
         {
-            model: db.assigned_teacher,
-            required: true,
+          model: db.assigned_teacher,
+          required: true,
         },
         {
-            model: db.recommended_audience,
-            required: true,
+          model: db.recommended_audience,
+          required: true,
         },
         {
-            model: db.assigned_discipline,
+          model: db.assigned_discipline,
+          required: true,
+          include: {
+            model: db.specialty,
             required: true,
-            include: {
-                model: db.specialty,
-                required: true,
-            }
+          },
         },
-    ]
-    })
+      ],
+    });
     const audiences = await db.audience.findAll({
-        include: {
-            model: db.assigned_audience,
-        }
-    })
+      include: {
+        model: db.assigned_audience,
+      },
+    });
+    const groups = await db.group.findAll();
 
-    populations = Init(classes, population_size, max_day, max_pair, audiences);
-  }
-}
+    let populations = Init(
+      classes,
+      population_size,
+      max_day,
+      max_pair,
+      audiences
+    );
+
+    populations.forEach((individ) => {
+      fitnessByGroups(individ, groups, classes, max_day, max_pair);
+    });
+  },
+};
