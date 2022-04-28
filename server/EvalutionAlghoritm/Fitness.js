05581597d9496c0b78f7871de93f033dd881905e
Kirill13 <kirillmedvedev279@gmail.com>
2022-04-28 12:41:42 +0300

    Fitnness

--- server/EvalutionAlghoritm/Fitness.js
+++ server/EvalutionAlghoritm/Fitness.js
@@ -1,7 +1,7 @@
 export default function fitness(individ, groups, classes, teachers) {
   let fitnessValue = 0;
   fitnessValue += fitnessByGroups(individ, groups, classes);
-  fitnessByTeachers(individ, teachers, classes);
+  fitnessValue += fitnessByTeachers(individ, teachers, classes);
   return fitnessValue;
 }
 
@@ -19,63 +19,104 @@ function fitnessByGroups(individ, groups, classes) {
         return schedule;
       }
     });
-    detectedSchedules.sort(function (schedule1, schedule2) {
-      if (schedule1.id_day_week > schedule2.id_day_week) return 1;
-      else if (schedule1.id_day_week == schedule2.id_day_week) {
-        if (schedule1.number_pair > schedule2.number_pair) return 1;
-        else if (schedule1.number_pair == schedule2.number_pair) {
-          if (schedule1.id_pair_type > schedule2.id_pair_type) return 1;
-          else if (schedule1.id_pair_type == schedule2.id_pair_type) {
-          } else return -1;
-        } else return -1;
-      } else return -1;
-    });
-    let index = 1;
-    while (index < detectedSchedules.length) {
-      if (
-        detectedSchedules[index - 1].id_day_week ==
-          detectedSchedules[index].id_day_week &&
-        detectedSchedules[index - 1].number_pair !=
-          detectedSchedules[index].number_pair
-      ) {
-        if (
-          detectedSchedules[index - 1].pair_type ==
-            detectedSchedules[index].pair_type ||
-          detectedSchedules[index].pair_type == 3
-        ) {
-          if (index > 1) {
-            if (
-              detectedSchedules[index].pair_type == 3 &&
-              detectedSchedules[index - 2].number_pair ==
-                detectedSchedules[index - 1].number_pair &&
-              detectedSchedules[index - 2].id_day_week ==
-                detectedSchedules[index].id_day_week
-            ) {
-              fitnessValue +=
-                detectedSchedules[index].number_pair -
-                detectedSchedules[index - 2].number_pair -
-                1;
-            }
-          }
-          fitnessValue +=
-            detectedSchedules[index].number_pair -
-            detectedSchedules[index - 1].number_pair -
-            1;
-        }
-      }
-
-      index++;
-      if (index < detectedSchedules.length)
-        if (
-          detectedSchedules[index].id_day_week ==
-            detectedSchedules[index - 1].id_day_week &&
-          detectedSchedules[index].number_pair ==
-            detectedSchedules[index - 1].number_pair
-        )
-          index++;
-    }
+    detectedSchedules = sortDS(detectedSchedules);
+    fitnessValue += fitnessDSWindows(detectedSchedules);
+    fitnessValue +=
+      detectedSchedules.length == 1
+        ? detectedSchedules[0].number_pair
+        : fitnessDSLateSchedule(detectedSchedules);
   });
   return fitnessValue;
+}
+function sortDS(detectedSchedules) {
+  detectedSchedules.sort(function (schedule1, schedule2) {
+    if (schedule1.id_day_week > schedule2.id_day_week) return 1;
+    else if (schedule1.id_day_week == schedule2.id_day_week) {
+      if (schedule1.number_pair > schedule2.number_pair) return 1;
+      else if (schedule1.number_pair == schedule2.number_pair) {
+        if (schedule1.id_pair_type > schedule2.id_pair_type) return 1;
+        else if (schedule1.id_pair_type == schedule2.id_pair_type) {
+        } else return -1;
+      } else return -1;
+    } else return -1;
+  });
+  return detectedSchedules;
+}
+function fitnessDSWindows(detectedSchedules) {
+  let fitnessValue = 0;
+  let index = 1;
+  while (index < detectedSchedules.length) {
+    if (
+      detectedSchedules[index - 1].id_day_week ==
+        detectedSchedules[index].id_day_week &&
+      detectedSchedules[index - 1].number_pair !=
+        detectedSchedules[index].number_pair
+    ) {
+      if (
+        detectedSchedules[index - 1].pair_type ==
+          detectedSchedules[index].pair_type ||
+        detectedSchedules[index].pair_type == 3
+      ) {
+        if (index > 1) {
+          if (
+            detectedSchedules[index].pair_type == 3 &&
+            detectedSchedules[index - 2].number_pair ==
+              detectedSchedules[index - 1].number_pair &&
+            detectedSchedules[index - 2].id_day_week ==
+              detectedSchedules[index].id_day_week
+          ) {
+            fitnessValue +=
+              detectedSchedules[index].number_pair -
+              detectedSchedules[index - 2].number_pair -
+              1;
+          }
+        }
+        fitnessValue +=
+          detectedSchedules[index].number_pair -
+          detectedSchedules[index - 1].number_pair -
+          1;
+      }
+    }
+    index++;
+    if (index < detectedSchedules.length)
+      if (
+        detectedSchedules[index].id_day_week ==
+          detectedSchedules[index - 1].id_day_week &&
+        detectedSchedules[index].number_pair ==
+          detectedSchedules[index - 1].number_pair
+      )
+        index++;
+  }
+  return fitnessValue;
+}
+
+function fitnessDSLateSchedule(detectedSchedules) {
+  let fitnessValue = 0;
+  let index = 1;
+  while (index < detectedSchedules.length) {
+    fitnessValue += detectedSchedules[index - 1].number_pair;
+    while (
+      detectedSchedules[index - 1].id_day_week ==
+      detectedSchedules[index].id_day_week
+    ) {
+      index++;
+      if (index == detectedSchedules.length) break;
+    }
+    index++;
+    if (index == detectedSchedules.length) break;
+  }
+  if (
+    detectedSchedules[detectedSchedules.length - 1].id_day_week !=
+    detectedSchedules[detectedSchedules.length - 2].id_day_week
+  )
+    fitnessValue += detectedSchedules[detectedSchedules.length - 1].number_pair;
+  return fitnessValue;
+}
+function fitnessEquelSchedule(detectedSchedules) {
+  let max = 0;
+  let min = 0;
+  let temp = 0;
+  let index = 0;
 }
 
 function fitnessByTeachers(individ, teachers, classes) {
@@ -94,6 +135,12 @@ function fitnessByTeachers(individ, teachers, classes) {
         return schedule;
       }
     });
-    console.log("hello");
+    detectedSchedules = sortDS(detectedSchedules);
+    fitnessValue += fitnessDSWindows(detectedSchedules);
+    fitnessValue +=
+      detectedSchedules.length == 1
+        ? detectedSchedules[0].number_pair
+        : fitnessDSLateSchedule(detectedSchedules);
   });
+  return fitnessValue;
 }
