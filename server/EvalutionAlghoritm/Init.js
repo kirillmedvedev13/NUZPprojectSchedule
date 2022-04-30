ed8c03bc04a1935888ec1128e8312710076704ac
Vlad Primakov <primakov2002@gmail.com>
2022-04-30 12:29:25 +0300

    123

--- server/EvalutionAlghoritm/Init.js
+++ server/EvalutionAlghoritm/Init.js
@@ -1,9 +1,11 @@
-import GetIdAudienceForClass from "./GetIdAudienceForClass.js";
+import GetIdAudienceForClassLecture from "./GetIdAudienceForClassLecture.js";
+import GetIdAudienceForClassPractice from "./GetIdAudienceForClassPractice.js";
 import GetRndInteger from "./GetRndInteger.js";
 import GetPairTypeForClass from "./GetPairTypeForClass.js";
-import CheckPutClassLectureForGroup from "./CheckPutClassLectureForGroup.js";
-import CheckPutClassPracticeForGroup from "./CheckPutClassPracticeForGroup.js";
+import CheckPutClassForGroupLecture from "./CheckPutClassForGroupLecture.js";
+import CheckPutClassForGroupPractice from "./CheckPutClassForGroupPractice.js";
 import CheckPutClassForTeacher from "./CheckPutClassForTeacher.js";
+import CheckPutClassForAudience from "./CheckPutClassForAudience.js";
 export default function (
   classes,
   population_size,
@@ -17,20 +19,20 @@ export default function (
   for (let i = 0; i < population_size; i++) {
     let schedule = [];
     classes.forEach((clas) => {
-      // Случайная вставка в расписание
+      // Случайная вставка в расписание - где возвращается массив, каждая ячейка которого парность в расписании
       const info = GetPairTypeForClass(clas);
       // Сколько раз вставлять данное занятие в разное время
       for (let j = 0; j < info.length; j++) {
         let isPut = false;
         while (!isPut) {
-          const id_audience = GetIdAudienceForClass(clas, audiences);
           // Если лекция то для всех групп в одно и тоже время
           if (clas.id_type_class === 1) {
             const day_week = GetRndInteger(1, max_day);
             const number_pair = GetRndInteger(1, max_pair);
+            const id_audience = GetIdAudienceForClassLecture(clas, audiences);
             //Если в это время нету пары для всех групп
             if (
-              CheckPutClassLectureForGroup(
+              CheckPutClassForGroupLecture(
                 clas,
                 schedule,
                 day_week,
@@ -45,7 +47,7 @@ export default function (
                 number_pair,
                 info[j],
                 mapTeacherAndAG
-              )
+              ) && CheckPutClassForAudience(id_audience, schedule,day_week,number_pair,info[j])
             ) {
               clas.assigned_groups.map((ag) => {
                 schedule.push({
@@ -62,13 +64,14 @@ export default function (
           // Если практика то для каждой группы своё время
           else if (clas.id_type_class === 2) {
             clas.assigned_groups.map((ag) => {
-              let isPutZnam = false;
-              while (!isPutZnam) {
+              let isPutPractice = false;
+              while (!isPutPractice) {
                 const day_week = GetRndInteger(1, max_day);
                 const number_pair = GetRndInteger(1, max_pair);
+                const id_audience = GetIdAudienceForClassPractice(ag.group, clas, audiences);
                 // Если в это время нету пары для конкретной группы
                 if (
-                  CheckPutClassPracticeForGroup(
+                  CheckPutClassForGroupPractice(
                     ag.id_group,
                     schedule,
                     day_week,
@@ -83,7 +86,7 @@ export default function (
                     number_pair,
                     info[j],
                     mapTeacherAndAG
-                  )
+                  ) && CheckPutClassForAudience(id_audience, schedule,day_week,number_pair,info[j])
                 ) {
                   schedule.push({
                     number_pair,
@@ -92,7 +95,7 @@ export default function (
                     id_audience,
                     id_assigned_group: ag.id,
                   });
-                  isPutZnam = true;
+                  isPutPractice = true;
                 }
               }
               isPut = true;
