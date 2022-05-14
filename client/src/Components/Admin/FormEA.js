e022eb868cc951a72a1133b2b513cf8fc1826202
Kirill13 <kirillmedvedev279@gmail.com>
2022-05-14 19:34:33 +0300

    Some fix

--- client/src/Components/Admin/FormEA.js
+++ client/src/Components/Admin/FormEA.js
@@ -1,34 +1,43 @@
 import React from "react";
-import { Form, Row, Col, Button, } from "react-bootstrap";
+import { Form, Row, Col, Button } from "react-bootstrap";
 import { useQuery, useMutation } from "@apollo/client";
 import { GET_INFO } from "./queries.js";
-import { UPDATE_INFO } from "./mutations.js"
-import { CreateNotification } from "../Alert.js"
+import { UPDATE_INFO } from "./mutations.js";
+import { CreateNotification } from "../Alert.js";
 
-function UpdateInfo({ info }) {
-  const [update_info, { loading, error }] = useMutation(UPDATE_INFO, { refetchQueries: GET_INFO });
+function ButtonUpdateInfo({ info }) {
+  const [UpdateInfo, { loading, error }] = useMutation(UPDATE_INFO, {
+    refetchQueries: GET_INFO,
+  });
   if (loading) return null;
   if (error) return `Error! ${error}`;
-  console.log(info)
+  let data = [];
+  for (const [key, value] of Object.entries(info)) {
+    if (value !== null) data.push({ key, value });
+  }
   return (
     <div className="my-2 mx-2 d-flex justify-content-center">
-      <Button onClick={(e) => {
-        update_info(info).then((res) => {
-          CreateNotification(res.data.UpdateInfo);
-        })
-      }
-      }
+      <Button
+        onClick={(e) => {
+          if (data.length === 0) return;
+          else
+            UpdateInfo({ variables: { data: JSON.stringify(data) } }).then(
+              (res) => {
+                CreateNotification(res.data.UpdateInfo);
+              }
+            );
+        }}
       >
-        Оновити данi</Button>
+        Оновити данi
+      </Button>
     </div>
-  )
+  );
 }
 
 function DataForm({ handleChangeInfo, handleSetInfo }) {
   const { loading, error, data } = useQuery(GET_INFO);
   if (loading) return null;
   if (error) return `Error! ${error}`;
-  console.log(data.GetInfo)
 
   return (
     <div onLoad={(e) => handleSetInfo(data.GetInfo)}>
@@ -36,13 +45,13 @@ function DataForm({ handleChangeInfo, handleSetInfo }) {
         <Form.Label className="col-5">Розмір популяції</Form.Label>
         <Col>
           <Form.Control
-            value={data.GetInfo.population_size}
+            defaultValue={data.GetInfo.population_size}
             type="number"
             min={0}
             onChange={(e) => {
               handleChangeInfo(
                 "population_size",
-                e ? Number(e.target.value) : 0
+                e ? Number(e.target.value) : null
               );
             }}
           />
@@ -52,13 +61,13 @@ function DataForm({ handleChangeInfo, handleSetInfo }) {
         <Form.Label className="col-5">Кількість популяцій</Form.Label>
         <Col>
           <Form.Control
-            value={data.GetInfo.max_generations}
+            defaultValue={data.GetInfo.max_generations}
             type="number"
             min={0}
             onChange={(e) => {
               handleChangeInfo(
                 "max_generations",
-                e ? Number(e.target.value) : 0
+                e ? Number(e.target.value) : null
               );
             }}
           />
@@ -68,14 +77,15 @@ function DataForm({ handleChangeInfo, handleSetInfo }) {
         <Form.Label className="col-5">Ймовірність схрещування</Form.Label>
         <Col>
           <Form.Control
-            value={data.GetInfo.p_crossover}
+            defaultValue={data.GetInfo.p_crossover}
             type="number"
             min={0}
             max={1}
             step={0.05}
             onChange={(e) => {
               handleChangeInfo(
-                "p_crossover", e ? Number(e.target.value) : 0
+                "p_crossover",
+                e ? Number(e.target.value) : null
               );
             }}
           />
@@ -85,13 +95,13 @@ function DataForm({ handleChangeInfo, handleSetInfo }) {
         <Form.Label className="col-5">Ймовірність мутації</Form.Label>
         <Col>
           <Form.Control
-            value={data.GetInfo.p_mutation}
+            defaultValue={data.GetInfo.p_mutation}
             type="number"
             min={0}
             max={1}
             step={0.05}
             onChange={(e) => {
-              handleChangeInfo("p_mutation", e ? Number(e.target.value) : 0);
+              handleChangeInfo("p_mutation", e ? Number(e.target.value) : null);
             }}
           />
         </Col>
@@ -100,13 +110,13 @@ function DataForm({ handleChangeInfo, handleSetInfo }) {
         <Form.Label className="col-5">Ймовірність мутації генів</Form.Label>
         <Col>
           <Form.Control
-            value={data.GetInfo.p_genes}
+            defaultValue={data.GetInfo.p_genes}
             type="number"
             min={0}
             max={1}
             step={0.001}
             onChange={(e) => {
-              handleChangeInfo("p_genes", e ? Number(e.target.value) : 0);
+              handleChangeInfo("p_genes", e ? Number(e.target.value) : null);
             }}
           />
         </Col>
@@ -115,13 +125,13 @@ function DataForm({ handleChangeInfo, handleSetInfo }) {
         <Form.Label className="col-5">Вікно в групи</Form.Label>
         <Col>
           <Form.Control
-            value={data.GetInfo.penaltyGrWin}
+            defaultValue={data.GetInfo.penaltyGrWin}
             type="number"
             min={0}
             onChange={(e) => {
               handleChangeInfo(
                 "penaltyGrWin",
-                e ? Number(e.target.value) : 0
+                e ? Number(e.target.value) : null
               );
             }}
           />
@@ -131,13 +141,13 @@ function DataForm({ handleChangeInfo, handleSetInfo }) {
         <Form.Label className="col-5">Вікно в викладача</Form.Label>
         <Col>
           <Form.Control
-            value={data.GetInfo.penaltyTeachWin}
+            defaultValue={data.GetInfo.penaltyTeachWin}
             type="number"
             min={0}
             onChange={(e) => {
               handleChangeInfo(
                 "penaltyTeachWin",
-                e ? Number(e.target.value) : 0
+                e ? Number(e.target.value) : null
               );
             }}
           />
@@ -147,13 +157,13 @@ function DataForm({ handleChangeInfo, handleSetInfo }) {
         <Form.Label className="col-5">Пізні заняття</Form.Label>
         <Col>
           <Form.Control
-            value={data.GetInfo.penaltyLateSc}
+            defaultValue={data.GetInfo.penaltyLateSc}
             type="number"
             min={0}
             onChange={(e) => {
               handleChangeInfo(
                 "penaltyLateSc",
-                e ? Number(e.target.value) : 0
+                e ? Number(e.target.value) : null
               );
             }}
           />
@@ -163,13 +173,13 @@ function DataForm({ handleChangeInfo, handleSetInfo }) {
         <Form.Label className="col-5">Рівномірний розклад</Form.Label>
         <Col>
           <Form.Control
-            value={data.GetInfo.penaltyEqSc}
+            defaultValue={data.GetInfo.penaltyEqSc}
             type="number"
             min={0}
             onChange={(e) => {
               handleChangeInfo(
                 "penaltyEqSc",
-                e ? Number(e.target.value) : 0
+                e ? Number(e.target.value) : null
               );
             }}
           />
@@ -179,30 +189,33 @@ function DataForm({ handleChangeInfo, handleSetInfo }) {
         <Form.Label className="col-5">Накладання занять</Form.Label>
         <Col>
           <Form.Control
-            value={data.GetInfo.penaltySameTimesSc}
+            defaultValue={data.GetInfo.penaltySameTimesSc}
             type="number"
             min={0}
             onChange={(e) => {
               handleChangeInfo(
                 "penaltySameTimesSc",
-                e ? Number(e.target.value) : 0
+                e ? Number(e.target.value) : null
               );
             }}
           />
         </Col>
       </Form.Group>
     </div>
-  )
+  );
 }
 
 export default class FormEA extends React.Component {
   render() {
     const { handleChangeInfo, handleSetInfo, info } = this.props;
-    console.log(info)
+    console.log(info);
     return (
       <Form>
-        <DataForm handleChangeInfo={handleChangeInfo} handleSetInfo={handleSetInfo}></DataForm>
-        <UpdateInfo info={info}></UpdateInfo>
+        <DataForm
+          handleChangeInfo={handleChangeInfo}
+          handleSetInfo={handleSetInfo}
+        ></DataForm>
+        <ButtonUpdateInfo info={info}></ButtonUpdateInfo>
       </Form>
     );
   }
--- client/src/Components/Admin/mutations.js
+++ client/src/Components/Admin/mutations.js
@@ -1,35 +1,12 @@
 import { gql } from "@apollo/client";
 
 export const UPDATE_INFO = gql`
-  mutation (
-    $population_size: Int!,
-    $max_generations: Int!,
-    $p_crossover: Double!,
-    $p_mutation: Double!,
-    $p_genes: Double!,
-    $penaltyGrWin: Double!,
-    $penaltyTeachWin: Double!,
-    $penaltyLateSc: Double!,
-    $penaltyEqSc: Double!,
-    $penaltySameTimesSc: Double!
-    ){
-      UpdateInfo(
-        population_size: $population_size,
-        max_generations: $max_generations,
-        p_crossover: $p_crossover,
-        p_mutation: $p_mutation,
-        p_genes: $p_genes,
-        penaltyGrWin: $penaltyGrWin,
-        penaltyTeachWin: $penaltyTeachWin,
-        penaltyLateSc: $penaltyLateSc,
-        penaltyEqSc: $penaltyEqSc,
-        penaltySameTimesSc: $penaltySameTimesSc
-      )
-      {
-        message
-        successful
-      }
+  mutation ($data: String!) {
+    UpdateInfo(data: $data) {
+      message
+      successful
     }
+  }
 `;
 
 export const SET_CLASSES = gql`
