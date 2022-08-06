2e98487ddb3b6c76459216a8ee325a6354b84e40
Kirill13 <kirillmedvedev279@gmail.com>
2022-08-06 12:05:54 +0300

    125

--- server/EvalutionAlgorithmCpp/EvalutionAlgoritmCpp/EvulationAlgoritmCpp/SortSchedule.h
+++
@@ -1,9 +0,0 @@
-#ifndef SORTSCHEDULE_H
-#define SORTSCHEDULE_H
-#include "json.hpp"
-#include <vector>
-using namespace std;
-using namespace nlohmann;
-void SortSchedule(vector<json> &schedule);
-
-#endif // SORTSCHEDULE_H
