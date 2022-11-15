a78bd226ba8da616b7e0a210446554aa2f869629
Kyrylo <kirillmedvedev279@gmail.com>
2022-11-15 14:30:31 +0200

    Add MultiCharts

--- client/src/Components/Admin/Algorithms/SelectAlgorithm.js
+++ client/src/Components/Admin/Algorithms/SelectAlgorithm.js
@@ -2,16 +2,13 @@ import React from "react";
 import { useQuery } from "@apollo/client";
 import Select from "react-select";
 import { GET_INFO } from "../queries";
+import GetLabelForAlgoritms from "./GetLabelForAlgoritms";
 
 export default function SelectAlgoritm({ handleChangeState }) {
   const { error, loading, data } = useQuery(GET_INFO);
   if (loading) return "Loading...";
   if (error) return `Error! ${error}`;
-  let options = [
-    { label: "Алгоритм простого перебору", value: "simple_algorithm" },
-    { value: "simulated_annealing", label: "Алгоритм імітації отжига" },
-    { value: "evolution_algorithm", label: "Генетичний алгоритм" },
-  ];
+  let options = GetLabelForAlgoritms();
   return (
     <Select
       className="col-12 my-2"
