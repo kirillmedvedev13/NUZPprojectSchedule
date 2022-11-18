a42af45fc7a38d74cf506006422041ab3f81f5fc
Vlad Primakov <primakov2002@gmail.com>
2022-11-18 23:22:58 +0200

    123

--- server/Algorithms/EvalutionAlgorithmCpp/Main.js
+++ server/Algorithms/EvalutionAlgorithmCpp/Main.js
@@ -1,6 +1,6 @@
 import MessageType from "../../Schema/TypeDefs/MessageType.js";
 import { exec, execFile, spawnSync } from "child_process";
-import fs from "fs";
+import fs, { readFileSync } from "fs";
 import { GraphQLInt, __DirectiveLocation } from "graphql";
 import SpawnChild from "./SpawnChild.js";
 import path from "path";
@@ -13,41 +13,52 @@ export const RUN_EACPP = {
     id_cathedra: { type: GraphQLInt },
   },
   async resolve(parent, { id_cathedra }) {
-    let {
-      max_day,
-      max_pair,
-      classes,
-      recommended_schedules,
-      evolution_values,
-      general_values,
-      audiences,
-      results,
-    } = await GetDataFromDB(id_cathedra);
+    try {
+      let {
+        max_day,
+        max_pair,
+        classes,
+        recommended_schedules,
+        evolution_values,
+        general_values,
+        audiences,
+        results,
+      } = await GetDataFromDB(id_cathedra);
 
-    let base_schedule = await ParseScheduleFromDB(id_cathedra);
+      let base_schedule = await ParseScheduleFromDB(id_cathedra);
 
-    let jsonData = JSON.stringify({
-      max_day,
-      max_pair,
-      evolution_values,
-      base_schedule,
-      recommended_schedules,
-      classes,
-      general_values,
-      audiences,
-    });
+      let jsonData = JSON.stringify({
+        max_day,
+        max_pair,
+        evolution_values,
+        base_schedule,
+        recommended_schedules,
+        classes,
+        general_values,
+        audiences,
+      });
 
-    let fileName = path.resolve(
-      "./Algorithms/EvalutionAlgorithmCpp/EvalutionAlgorithmCpp.exe"
-    );
-    let fileData = path.resolve("./Algorithms/EvalutionAlgorithmCpp/");
-    fs.writeFileSync(fileData + "/data.json", jsonData, "utf-8", (err) => {
-      if (err) console.log(err);
-    });
+      let fileName = path.resolve(
+        "./Algorithms/EvalutionAlgorithmCpp/EvalutionAlgorithmCpp.exe"
+      );
+      let fileData = path.resolve("./Algorithms/EvalutionAlgorithmCpp/");
+      fs.writeFileSync(fileData + "/data.json", jsonData, (err) => {
+        if (err) console.log(err);
+      });
 
-    const promise = SpawnChild(fileName);
-    promise.then((code) => {
-      console.log(code);
-    })
-  },
+      const promise = SpawnChild(fileName);
+      promise.then((code) => {
+        if (code === 0) {
+          let res = readFileSync("./Algorithms/EvalutionAlgorithmCpp/result.json");
+          res = JSON.parse(res);
+          let bestPopulation = res.bestPopulation;
+          let result = res.evolution_algorithmCPP;
+
+        }
+      })
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
 };
