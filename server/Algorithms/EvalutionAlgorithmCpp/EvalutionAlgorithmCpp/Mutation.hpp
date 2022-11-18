e9f2026330394e93efb4cd9b2be9850595ff73c1
Vlad Primakov <primakov2002@gmail.com>
2022-11-18 21:48:20 +0200

    123

--- server/Algorithms/EvalutionAlgorithmCpp/EvalutionAlgorithmCpp/Mutation.hpp
+++ server/Algorithms/EvalutionAlgorithmCpp/EvalutionAlgorithmCpp/Mutation.hpp
@@ -6,41 +6,70 @@
 #include "GetIdAudienceForClass.hpp"
 #include "GetRndInteger.hpp"
 
-void Mutation(vector<individ> &populations,const int &index, const double& p_genes, const int& max_day, const int& max_pair, vector<audience>& audiences, vector<clas>& classes)
+void Mutation(vector<individ> &populations,const int &index, const int& max_day, const int& max_pair, vector<audience>& audiences, vector<clas>& classes)
 {
-    for (auto &cl : classes){
-        if (GetRndDouble() <= p_genes){
-            for(int i =0; i < (int)cl.schedules[index].size(); i++){
-                // С шансом 50 = будет менятся каждая пара в занятие
-                if(GetRndDouble() <= 0.5){
-                    // если нету рекомендуемое время, то пару не менять
-                    int s =cl.recommended_schedules.size()-1;
-                    if(i > s){
-                        int day_week = GetRndInteger(1, max_day);
-                        int number_pair = GetRndInteger(1,max_pair);
-                        int new_id_audience = GetIdAudienceForClass(cl, audiences);
-                        int old_id_audience = cl.schedules[index][i].id_audience;
-                        int pair_type = cl.schedules[index][i].pair_type;
-                        // С шансом 50 менять числитель на знаменатель
-                        if(pair_type < 3 && GetRndDouble() <= 0.5){
-                            pair_type = 3 - pair_type;
-                        }
-                        cl.schedules[index][i].day_week = day_week;
-                        cl.schedules[index][i].number_pair = number_pair;
-                        cl.schedules[index][i].pair_type = pair_type;
-                        // Замена ссылки для аудитории
-                        if (old_id_audience != new_id_audience){
-                            cl.schedules[index][i].id_audience = new_id_audience;
-                            auto ref = &cl.schedules[index][i];
-                            auto it = find(populations[index].scheduleForAudiences[old_id_audience].begin(), populations[index].scheduleForAudiences[old_id_audience].end(), ref);
-                            populations[index].scheduleForAudiences[old_id_audience].erase(it);
-                            populations[index].scheduleForAudiences[new_id_audience].emplace(populations[index].scheduleForAudiences[new_id_audience].begin(), ref);
-                        }
-                    }
-                }
-            }
+    int rClasses = GetRndInteger(0,classes.size()-1);
+    auto &cl = classes[rClasses];
+    int i = GetRndInteger(0,cl.schedules[index].size()-1);
+    // если нету рекомендуемое время, то пару не менять
+    int s =cl.recommended_schedules.size()-1;
+    if(i > s){
+        int day_week = GetRndInteger(1, max_day);
+        int number_pair = GetRndInteger(1,max_pair);
+        int new_id_audience = GetIdAudienceForClass(cl, audiences);
+        int old_id_audience = cl.schedules[index][i].id_audience;
+        int pair_type = cl.schedules[index][i].pair_type;
+        // С шансом 50 менять числитель на знаменатель
+        if(pair_type < 3 && GetRndDouble() <= 0.5){
+            pair_type = 3 - pair_type;
+        }
+        cl.schedules[index][i].day_week = day_week;
+        cl.schedules[index][i].number_pair = number_pair;
+        cl.schedules[index][i].pair_type = pair_type;
+        // Замена ссылки для аудитории
+        if (old_id_audience != new_id_audience){
+            cl.schedules[index][i].id_audience = new_id_audience;
+            auto ref = &cl.schedules[index][i];
+            auto it = find(populations[index].scheduleForAudiences[old_id_audience].begin(), populations[index].scheduleForAudiences[old_id_audience].end(), ref);
+            populations[index].scheduleForAudiences[old_id_audience].erase(it);
+            populations[index].scheduleForAudiences[new_id_audience].emplace(populations[index].scheduleForAudiences[new_id_audience].begin(), ref);
+
         }
     }
 }
+//    for (auto &cl : classes){
+//        if (GetRndDouble() <= p_genes){
+//            for(int i =0; i < (int)cl.schedules[index].size(); i++){
+//                // С шансом 50 = будет менятся каждая пара в занятие
+//                if(GetRndDouble() <= 0.5){
+//                    // если нету рекомендуемое время, то пару не менять
+//                    int s =cl.recommended_schedules.size()-1;
+//                    if(i > s){
+//                        int day_week = GetRndInteger(1, max_day);
+//                        int number_pair = GetRndInteger(1,max_pair);
+//                        int new_id_audience = GetIdAudienceForClass(cl, audiences);
+//                        int old_id_audience = cl.schedules[index][i].id_audience;
+//                        int pair_type = cl.schedules[index][i].pair_type;
+//                        // С шансом 50 менять числитель на знаменатель
+//                        if(pair_type < 3 && GetRndDouble() <= 0.5){
+//                            pair_type = 3 - pair_type;
+//                        }
+//                        cl.schedules[index][i].day_week = day_week;
+//                        cl.schedules[index][i].number_pair = number_pair;
+//                        cl.schedules[index][i].pair_type = pair_type;
+//                        // Замена ссылки для аудитории
+//                        if (old_id_audience != new_id_audience){
+//                            cl.schedules[index][i].id_audience = new_id_audience;
+//                            auto ref = &cl.schedules[index][i];
+//                            auto it = find(populations[index].scheduleForAudiences[old_id_audience].begin(), populations[index].scheduleForAudiences[old_id_audience].end(), ref);
+//                            populations[index].scheduleForAudiences[old_id_audience].erase(it);
+//                            populations[index].scheduleForAudiences[new_id_audience].emplace(populations[index].scheduleForAudiences[new_id_audience].begin(), ref);
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
 
 #endif
