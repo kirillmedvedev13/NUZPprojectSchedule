3210419159ae9b0edba5891ab48420d270b65c1e
Kyrylo <kirillmedvedev279@gmail.com>
2023-03-06 13:01:38 +0200

    123

--- server/Algorithms/NeuralNetwork/ConvertJSONtoCSV.py
+++ server/Algorithms/NeuralNetwork/ConvertJSONtoCSV.py
@@ -5,77 +5,70 @@ import pandas
 import json
 
 
-def NormalizeClasses(classesJSON):
-    classesDict=[]
+def getLastIndex(dir):
+    index = 0
+    for file in listdir(dir):
+        index += 1
+    return index
 
-    for classes in classesJSON:
-        temp = {}
-        for clas in classes:
-            cl = {}
-            cl["id"] = clas["id"]
-            cl["id_type_class"] = clas["id_type_class"]
-            cl["id_assigned_discipline"] = clas["id_assigned_discipline"]
-            assigned_groups = []
-            for ag in clas["assigned_groups"]:
-                assigned_groups.append(ag["id_group"])
-            cl["assigned_groups"] = assigned_groups
-            assigned_teachers = []
-            for at in clas["assigned_teachers"]:
-                assigned_teachers.append(at["id_teacher"])
-            cl["assigned_teachers"]=assigned_teachers
-            temp[cl["id"]]=cl
-        classesDict.append(temp)
-        """dataFrame = pandas.DataFrame.from_records(temp)
-        dataFrame.to_csv("classes.csv")
-        print(dataFrame)"""
-    return classesDict
 
-mainDir = "D://PROJECT SCHEDULE//Project//NUZPprojectSchedule//server//Algorithms//NeuralNetwork//DatasetSchedulesJSON"
-classesJSON = []
-schedulesJSON = []
-for dir in listdir(mainDir):
-    classSchedules = []
-    dirPath = join(mainDir,dir)
-    for file in listdir(dirPath):
-        filePath = join(dirPath,file)
-        with open(filePath,'r') as f:
-            data = json.loads(f.read())
-            if "classes" in file:
-                classesJSON.append(data)
-            else:
-                classSchedules.append(data)
-    schedulesJSON.append(classSchedules)
-
-classesDict = NormalizeClasses(classesJSON)
-
+inputDir = "D://PROJECT SCHEDULE//Project//NUZPprojectSchedule//server//Algorithms//NeuralNetwork//DatasetSchedulesJSON"
 saveDir = "D://PROJECT SCHEDULE//Project//NUZPprojectSchedule//server//Algorithms//NeuralNetwork//DatasetSchedulesCSV"
 
 
-for i in range(len(schedulesJSON)):
-    
+lastIndex = getLastIndex(saveDir)
+for file in listdir(inputDir):
+    filePath = join(inputDir, file)
+    schedules = []
+    with open(filePath, 'r') as f:
+        data = json.loads(f.read())
+        for sched in data:
+            newSched = {}
+            newSched['day_week'] = sched["day_week"]
+            newSched["number_pair"] = sched["number_pair"]
+            newSched["pair_type"] = sched["pair_type"]
+            newSched["id_class"] = sched["class"]["id"]
+            newSched["id_type_class"] = sched["class"]["type_class"]["id"]
+            newSched["id_audience"] = sched["audience"]["id"]
+            groups = sched["class"]["assigned_groups"]
+            teachers = sched["class"]["assigned_teachers"]
+            for teacher in teachers:
+                for group in groups:
+                    copySched = copy(newSched)
+                    copySched["id_teacher"] = teacher["teacher"]["id"]
+                    copySched["id_group"] = group["group"]["id"]
+                    schedules.append(copySched)
+
+        schedules = sorted(schedules, key=lambda sch: (
+            sch["day_week"], sch["number_pair"], sch["pair_type"], sch["id_class"]))
+        pandas.DataFrame.from_records(schedules).to_csv(
+            saveDir+"//schedules"+str(lastIndex)+".csv", index=False)
+        lastIndex += 1
+
+
+'''for i in range(len(schedulesJSON)):
+
     for schedule in schedulesJSON[i]:
-        tempSched=[]
+        tempSched = []
         for sched in schedule:
             clas = classesDict[i][sched["id_class"]]
             newSched = {}
             newSched["day_week"] = sched["day_week"]
             newSched["number_pair"] = sched["number_pair"]
             newSched["pair_type"] = sched["pair_type"]
-            newSched["id_class"] =sched["id_class"]
-            newSched["id_type_class"]= clas["id_type_class"]
+            newSched["id_class"] = sched["id_class"]
+            newSched["id_type_class"] = clas["id_type_class"]
             newSched["id_audience"] = sched["id_audience"]
-            id_teachers =clas["assigned_teachers"]
+            id_teachers = clas["assigned_teachers"]
             id_groups = clas["assigned_groups"]
             for teach in id_teachers:
                 for group in id_groups:
-                    copSched = copy (newSched)
+                    copSched = copy(newSched)
                     copSched["id_teacher"] = teach
                     copSched["id_group"] = group
                     tempSched.append(copSched)
-        
-        tempSched = sorted(tempSched,key=lambda sch: (sch["day_week"],sch["number_pair"],sch["pair_type"],sch["id_class"]))    
-        pandas.DataFrame.from_records(tempSched).to_csv(saveDir+"//schedules"+str(i)+str(schedulesJSON[i].index(schedule))+".csv",index=False)
 
-       
-
-
+        tempSched = sorted(tempSched, key=lambda sch: (
+            sch["day_week"], sch["number_pair"], sch["pair_type"], sch["id_class"]))
+        pandas.DataFrame.from_records(tempSched).to_csv(
+            saveDir+"//schedules"+str(i)+str(schedulesJSON[i].index(schedule))+".csv", index=False)'''
--- server/Algorithms/NeuralNetwork/Main.py
+++ server/Algorithms/NeuralNetwork/Main.py
@@ -7,30 +7,48 @@ from keras.models import Sequential
 from keras.layers import LSTM, Dense
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
-def decodeData(schedule,day_week,number_pair):
+
+def decodeData(schedule, day_week, number_pair):
     newData = []
     for clas in schedule:
         maxEl = max(clas)
-        index = np.where(maxEl==clas)[0][0]
+        index = np.where(maxEl == clas)[0][0]
         dw = index // day_week+1
         npair = index % day_week+1
-        newData.append([dw,npair])
+        newData.append([dw, npair])
     return newData
 
-def encodeData(data,day_week,number_pair):
+
+def encodeData(data, day_week, number_pair):
     newData = []
     for list in data:
         newList = []
-        for [day,nump] in list:
+        for [day, nump] in list:
             tempArr = np.zeros((day_week*number_pair))
-            tempArr[(day-1)*number_pair + nump-1]=1
+            tempArr[(day-1)*number_pair + nump-1] = 1
             newList.append(tempArr)
         newData.append(newList)
     return np.array(newData)
 
 
-        
+def labelEncode(data):
+    encoders = {}
+    keys = ["id_class", "id_audience", "id_teacher",
+            "id_group", "pair_type", "id_type_class"]
+    for key in keys:
+        newEncoder = LabelEncoder()
+        data[key] = newEncoder.fit_transform(data[key])
+        encoders[key] = newEncoder
+    return [data, encoders]
+
+
+def labelDecode(data, encoders):
+    for [key, encoder] in encoders:
+        data[key] = encoder.inverse_transform(data[key])
+    return data
+
 
 day_week = 6
 number_pair = 7
@@ -42,25 +60,33 @@ mainDir = "D://PROJECT SCHEDULE//Project//NUZPprojectSchedule//server//Algorithm
 dataX = []
 dataY = []
 for file in listdir(mainDir):
-    fileData = pd.read_csv(join(mainDir,file))
-    fileDataX = fileData[["pair_type","id_class","id_type_class","id_audience","id_teacher","id_group"]]
-    fileDataY = fileData[["day_week","number_pair"]]
+    fileData = pd.read_csv(join(mainDir, file))
+    fileDataX = fileData[["pair_type", "id_class",
+                          "id_type_class", "id_audience", "id_teacher", "id_group"]]
+    fileDataX = labelEncode(fileDataX)[0]
+    fileDataY = fileData[["day_week", "number_pair"]]
     dataX.append(fileDataX)
     dataY.append(fileDataY)
 
-dataX =  pad_sequences(dataX, padding='post')
-dataY =  pad_sequences(dataY, padding='post')
-dataY = encodeData(dataY,day_week,number_pair)
+encoder = LabelEncoder()
 
-dataTrainX, dataTestX, dataTrainY,dataTestY = train_test_split(dataX, dataY, test_size = 0.3, random_state = 1)
-    
+dataX = pad_sequences(dataX, padding='post')
+
+dataY = pad_sequences(dataY, padding='post')
+dataY = encodeData(dataY, day_week, number_pair)
+
+dataTrainX, dataTestX, dataTrainY, dataTestY = train_test_split(
+    dataX, dataY, test_size=0.3, random_state=1)
+
 model = Sequential()
-model.add(LSTM(day_week*number_pair,dropout=0.2,  return_sequences=True,input_shape=(None,n_featuresX)))
+model.add(LSTM(day_week*number_pair, dropout=0.2,
+          return_sequences=True, input_shape=(None, n_featuresX)))
 model.add(Dense(day_week*number_pair, activation='softmax'))
 
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam', metrics=['accuracy'])
 
-history= model.fit(dataTrainX,dataTrainY,epochs=500,batch_size=1)
+history = model.fit(dataTrainX, dataTrainY, epochs=500, batch_size=1)
 
 history.history
 
@@ -68,5 +94,5 @@ results = model.evaluate(dataTestX, dataTestY, batch_size=1)
 print("test loss, test acc:", results)
 
 predicted_timetable = model.predict(dataTestX[:1],)
-schedule = decodeData(predicted_timetable[0],day_week,number_pair)
+schedule = decodeData(predicted_timetable[0], day_week, number_pair)
 print(schedule)
