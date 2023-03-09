3061ec4bae7ea72bc6ccdf0af7ff84c67f314965
VladyslavPrymakov <Primakov2002@gmail.com>
2023-03-09 18:20:56 +0200

    123

--- client/src/config.js
+++ client/src/config.js
@@ -1,10 +1,5 @@
 const config = {
-<<<<<<< HEAD
   HOST: "localhost",
-=======
-  HOST: "26.159.44.130",
-  //HOST: "localhost",
->>>>>>> 55cd0100aacab7cad822ee17ad516c74452abdb2
   PORT: "3002",
 };
 
