e5f7ccb5ccc3801758a95d6ff817f73f198afe5c
VladyslavPrymakov <Primakov2002@gmail.com>
2023-06-07 15:32:19 +0300

    1231231

---
+++ server/Algorithms/EvolutionAlgorithmsCPP/TESTS.js
@@ -0,0 +1,38 @@
+import { readFileSync, writeFileSync } from "fs";
+import SpawnChild from "../Service/SpawnChild.js";
+import path from "path";
+
+async function Run(){
+    let pathToAlgorithm = path.resolve("ClassicEvolutionAlgorithm.exe");
+    let pathToSA = path.resolve("../SimpleAlgorithmCPP/SimpleAlgorithmCPP.exe");
+    let pathToData= path.resolve("");
+    let test_params = readFileSync(pathToData + "\\params_test.json")
+    let data = readFileSync(pathToData + "\\data.json")
+    data = JSON.parse(data);
+    test_params = JSON.parse(test_params);
+    let i = 2;
+    for (let params of test_params.items){
+        console.log(`NUMBER TEST - ${i}`);
+        data.params = params;
+        writeFileSync(pathToData + "\\data.json", JSON.stringify(data));
+        let code;
+        if (params["type_initialization"] == "simple_algorithm") {
+            code = await SpawnChild(pathToAlgorithm, [pathToData, pathToSA]);
+        } else {
+            code = await SpawnChild(pathToAlgorithm, [pathToData]);
+        }
+        if (code === 0) {
+            let res = readFileSync(pathToData + "\\result.json");
+            writeFileSync(pathToData + `\\RESULTS\\result${i}.json`, res);
+        }
+        else{
+            throw "SOME ERROR WHEN TESTING"
+        }
+        i++;
+    }
+}
+try {
+    await Run();
+} catch (err) {
+    console.log(err);
+}
