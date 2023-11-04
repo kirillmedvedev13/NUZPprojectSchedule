8ff062992bba7bc140da0b97c8206b54c9e507fb
VladyslavPrymakov <Primakov2002@gmail.com>
2023-11-04 16:35:51 +0300

    123

--- client/src/index.js
+++ client/src/index.js
@@ -11,7 +11,7 @@ import "./App.css";
 import config from "./config.js";
 
 const client = new ApolloClient({
-  uri: `http://${config.HOST}:${config.PORT}/graphql`,
+  uri: `${config.HOST}:${config.PORT}/graphql`,
   cache: new InMemoryCache(),
 });
 
